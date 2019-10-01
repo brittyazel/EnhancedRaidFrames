6744e7ee43886e82955c78ca3afcf1339ea88f79
Britt Yazel <bwyazel@gmail.com>
2019-10-01 09:49:37 -0700

    Update License

--- RaidFrameIndicatorsConfig.lua
+++ RaidFrameIndicatorsConfig.lua
@@ -1,4 +1,25 @@
-﻿local Defaults = {}
+--Enhanced Raid Frame, a World of Warcraft® user interface addon.
+
+--This file is part of Enhanced Raid Frame.
+--
+--Enhanced Raid Frame is free software: you can redistribute it and/or modify
+--it under the terms of the GNU General Public License as published by
+--the Free Software Foundation, either version 3 of the License, or
+--(at your option) any later version.
+--
+--Enhanced Raid Frame is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+--GNU General Public License for more details.
+--
+--You should have received a copy of the GNU General Public License
+--along with this program.  If not, see <https://www.gnu.org/licenses/>.
+--
+--Copyright for portions of Neuron are held in the public domain,
+--as determined by Szandos. All other copyrights for
+--Enhanced Raid Frame are held by Britt Yazel, 2017-2019.
+
+local Defaults = {}
 local Options = {}
 
 local RaidFrameIndicators = RaidFrameIndicators_Global
