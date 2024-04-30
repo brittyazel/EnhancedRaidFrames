059f1a210dcdc2fc60e81da579f06fd96f554737
Britt Yazel <bwyazel@gmail.com>
2024-04-30 08:48:57 -0700

    Update copyright for 2024

--- Localizations/koKR.lua
+++ Localizations/koKR.lua
@@ -1,5 +1,5 @@
 ﻿-- Enhanced Raid Frames is a World of Warcraft® user interface addon.
--- Copyright (c) 2017-2023 Britt W. Yazel
+-- Copyright (c) 2017-2024 Britt W. Yazel
 -- This code is licensed under the MIT license (see LICENSE for details)
 
 local L = LibStub("AceLocale-3.0"):NewLocale("EnhancedRaidFrames", "koKR", false)
